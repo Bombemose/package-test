50d179947185213bdacd38380554d2c3763363d0
Bombemose <lars@birkmose-jensen.dk>
2016-04-25 21:18:26 +0200

    First version

---
+++ testpakke/TestKlasse.java
@@ -0,0 +1,38 @@
+package testpakke;
+
+
+/**
+ * Lav en beskrivelse af klassen TestKlasse her.
+ * 
+ * @author (dit navn her)
+ * @version (versions nummer eller dato her)
+ */
+public class TestKlasse
+{
+    // instansvariabler - erstat eksemplet herunder med dine egne variabler
+    private int x;
+
+    /**
+     * Konstruktør for objekter af klassen TestKlasse
+     */
+    public TestKlasse()
+    {
+        // initialiser instansvariable
+        x = 0;
+    }
+    
+    public void adderTilX(int y) {
+        x += y;
+    }
+
+    /**
+     * Et eksempel på en metode - erstat denne kommentar med din egen
+     * 
+     * @return    X 
+     */
+    public int returnerX()
+    {
+        // indsæt din egen kode her
+        return x;
+    }
+}
